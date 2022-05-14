e7dea28bef0a73e72b1d2b8c5e74c75b06f7ca7c
Furkan Elmas <furkanelmas321@gmail.com>
2022-05-14 18:45:40 +0300

    Day 1- Data Types

---
+++ Day 1- Data Types/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int i = 4;
+        double d = 4.0;
+        string s = "HackerRank ";
+
+        Console.WriteLine(i + Convert.ToInt32(Console.ReadLine()));
+        Console.WriteLine(string.Format("{0:0.0}", d + double.Parse(Console.ReadLine())));
+        Console.WriteLine(s + Console.ReadLine());
+
+
+
+    }
+}
