718dc926c6c91453754dec0f0281fc1b17b7ce20
Furkan Elmas <furkanelmas321@gmail.com>
2022-05-14 18:46:32 +0300

    Day 6- Let's Review

---
+++ Day 6- Let's Review/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        int strCount = Convert.ToInt32(Console.ReadLine().Trim());
+        string[] givenStrings = new string[strCount];
+        List<Char> evenIndChar = new List<Char>();
+        List<Char> oddIndChar = new List<Char>();
+
+        for (int i = 0; i < strCount; i++)
+        {
+            givenStrings[i] = Console.ReadLine().Trim();
+        }
+        foreach (string str in givenStrings)
+        {
+            for (int j = 0; j < str.Length; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    evenIndChar.Add(str[j]);
+                }
+                else if (j % 2 == 1)
+                {
+                    oddIndChar.Add(str[j]);
+                }
+            }
+            Console.Write(String.Join("", evenIndChar));
+            Console.Write(" ");
+            Console.Write(String.Join("", oddIndChar));
+            Console.WriteLine();
+            evenIndChar.Clear();
+            oddIndChar.Clear();
+        }
+
+
+    }
+}
