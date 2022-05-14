d714b907fbb4a8e02b9c61df6c45c2b95cc641d3
Furkan Elmas <furkanelmas321@gmail.com>
2022-05-14 18:46:00 +0300

    Day 9- Recursion 3

---
+++ Day 9- Recursion 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Text;
+using System;
+
+class Result
+{
+    public static int factorial(int n)
+    {
+        if (n < 2)
+            return n;
+        return factorial(n - 1) * n;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result.factorial(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
